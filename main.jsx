5970bda268a88709fffff1a4d5656d43c16b6381
sama-kamel2 <samashokry168@gmail.com>
2025-07-05 16:40:40 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DarkWhispers from './DarkWhispers';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DarkWhispers />
+  </React.StrictMode>
+);
